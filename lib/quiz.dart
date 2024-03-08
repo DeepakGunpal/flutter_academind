4c263fd86f1ab451d21e1bae973521c8f54532cc
DeepakGunpal <gunpaldeepak@gmail.com>
2024-03-08 23:37:23 +0530

    quiz app

---
+++ lib/quiz.dart
@@ -0,0 +1,62 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  Widget? activeScreen;
+  @override
+  void initState() {
+    // TODO: implement initState
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = ResultsScreen(
+          chosenAnswer: selectedAnswers,
+        );
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple,
+                Colors.purple,
+              ],
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/results_screen.dart
@@ -0,0 +1,57 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key, required this.chosenAnswer});
+
+  final List<String> chosenAnswer;
+
+  // List<Map<String, Object>> getSummaryData() {
+  // getter for the summary data
+  List<Map<String, Object>> get summaryData {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswer[i]
+      });
+    }
+
+    return summary;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final summaryData = getSummaryData(); // getSummaryData converted to summaryData getter
+    final totalQuestions = questions.length;
+    final correctAnswers = summaryData
+        .where((data) => data['user_answer'] == data['correct_answer'])
+        .length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "You answered $correctAnswers out of $totalQuestions questions correctly!"),
+            const SizedBox(height: 30),
+            QuestionsSummary(summaryData),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.refresh),
+              label: const Text("Restart Quiz"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
